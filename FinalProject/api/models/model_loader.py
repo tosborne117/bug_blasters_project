9a048f8c51dd6f257f8c283441b146fe2a5d7d46
Thomas <tosbor13@charlotte.edu>
2024-11-20 15:15:52 -0500

    Update model_loader.py

--- FinalProject/api/models/model_loader.py
+++ FinalProject/api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_details, recipes, sandwiches, resources, reviews
 
 from ..dependencies.database import engine
 
@@ -9,3 +9,4 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    reviews.Base.metadata.create_all(engine)
