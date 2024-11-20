751c34c64b02cd60431e6b8325403a777f9863c7
Talha Kavaz <talhakavaz61@gmail.com>
2024-11-20 15:29:26 -0500

    sandwiches model

---
+++ FinalProject/api/models/customer.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    phone_number = Column(String(20), nullable=False)
+    address = Column(String(255), nullable=True)
