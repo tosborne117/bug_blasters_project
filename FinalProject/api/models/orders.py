8032fd1a818dcd1627391d00d3046d45562a436b
Daniel Ferrer <dferrer@uncc.edu>
2024-11-22 14:30:08 -0500

    fix: Modified orders model and schemas

--- FinalProject/api/models/orders.py
+++ FinalProject/api/models/orders.py
@@ -1,15 +1,18 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
-from datetime import datetime
 from ..dependencies.database import Base
 
 
 class Order(Base):
     __tablename__ = "orders"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    customer_name = Column(String(100))
-    order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
-    description = Column(String(300))
 
+    order_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    customer_name = Column(String(100), nullable=False)
+    order_date = Column(DATETIME, nullable=False, server_default=func.now())
+    description = Column(String(300))
+    payment_id = Column(Integer, ForeignKey("payment.id"), nullable=False)
+    tracking_num = Column(Integer, unique=True, nullable=False)
     order_details = relationship("OrderDetail", back_populates="order")
