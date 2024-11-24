31f75e4db589b40d5dd810f28d4e3461d7957287
CalebFilip <filip0732caleb@gmail.com>
2024-11-24 13:14:43 -0500

    fix: debugged to get uvicorn working

--- FinalProject/api/models/recipes.py
+++ FinalProject/api/models/recipes.py
@@ -10,7 +10,7 @@ class Recipe(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     sandwich_id = Column(Integer, ForeignKey("sandwiches.id"))
     resource_id = Column(Integer, ForeignKey("resources.id"))
-    amount = Column(Integer, index=True, nullable=False, server_default='0.0')
+    amount = Column(Integer, nullable=False, server_default='0.0')
 
     sandwich = relationship("Sandwich", back_populates="recipes")
     resource = relationship("Resource", back_populates="recipes")
