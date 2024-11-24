7bf748b7db3f39bb90d3dc140bc46c7709ed9ca7
CalebFilip <filip0732caleb@gmail.com>
2024-11-24 13:14:43 -0500

    fix: debugged to get uvicorn working

--- FinalProject/api/models/promotions.py
+++ FinalProject/api/models/promotions.py
@@ -9,7 +9,7 @@ class Promotions(Base):
     promotion_key = Column("promotion_key", Integer, primary_key=True, index=True)
     promotion_name = Column("promotion_name", String(100), index=True, nullable=False)
     start_date = Column("start_date", Date, index=True, nullable=False)
-    end_date = Column("end_date", Date, index="True", nullable=False)
-    percentage = Column("percentage", Integer, index="True", nullable="False")
+    end_date = Column("end_date", Date, nullable=False)
+    percentage = Column("percentage", Integer, nullable="False")
 
     order = relationship("Order", back_populates="promotion")
