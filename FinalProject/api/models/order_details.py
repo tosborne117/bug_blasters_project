240da34599556129f0c5e1b1892dbba62f08ad0b
Thomas <tosbor13@charlotte.edu>
2024-12-03 19:53:11 -0500

    Fix: Naming convention errors

--- FinalProject/api/models/order_details.py
+++ FinalProject/api/models/order_details.py
@@ -12,18 +12,12 @@ class OrderDetail(Base):
     amount = Column(Integer, index=True, nullable=False)
 
     sandwich = relationship("Sandwich", back_populates="order_details")
-    #order = relationship("Order", back_populates="order_details")
-
-
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
-from sqlalchemy.orm import relationship
-from ..dependencies.database import Base
-
+    order = relationship("Order", back_populates="order_details")
 
 class PaymentDetail(Base):
     __tablename__ = "payment_details"
 
     id = Column(Integer, primary_key=True, index=True)
     order_id = Column(Integer, ForeignKey("order_details.id", ondelete="CASCADE"), nullable=False)
-    amount = Column(Float, nullable=False)
+    amount = Column(DECIMAL(6,2), nullable=False)
     method = Column(String(50), nullable=False)
