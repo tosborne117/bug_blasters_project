0f3a81dc120e163a72c9dffe2d04a8d1eb84038b
Mena Tadesse <mena020924@gmail.com>
2024-12-05 15:16:25 -0500

    fix: updated read_one to retrieve a single order

--- FinalProject/api/controllers/orders.py
+++ FinalProject/api/controllers/orders.py
@@ -55,17 +55,17 @@ def read_all(db: Session):
 
 def read_one(db: Session, order_id: int):
     try:
-        order = db.query(order_model.Order).all()
+        order = db.query(order_model.Order).filter(order_model.Order.order_id == order_id).first()
         if not order:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         return {
             "order_id": order.order_id,
-                "customer_name": order.customer_name,
-                "order_date": order.order_date,
-                "description": order.description,
-                "payment_type": order.payment_type,
-                "promotion_key": order.promotion_key,
-                "order_status": order.order_status
+            "customer_name": order.customer_name,
+            "order_date": order.order_date,
+            "description": order.description,
+            "payment_type": order.payment_type,
+            "promotion_key": order.promotion_key,
+            "order_status": order.order_status
         }
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
